50e2950ec2998df928890474828da577b1dc9295
zaki-hanafiah <zaki@zakhov.com>
2024-08-10 00:19:25 +0800

    chore: update eslint rules and add sample vitest unit test config

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,11 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import pluginPrettier from 'eslint-plugin-prettier'
-import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from "eslint-config-prettier";
+import tsEslint from 'typescript-eslint'
 
-export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+export default tsEslint.config({
+  extends: [js.configs.recommended, ...tsEslint.configs.recommended],
   files: ['**/*.{ts,tsx}'],
   ignores: ['dist'],
   languageOptions: {
@@ -16,7 +17,7 @@ export default tseslint.config({
   plugins: {
     'react-hooks': reactHooks,
     'react-refresh': reactRefresh,
-    'prettier': pluginPrettier,
+    'plugin-prettier': pluginPrettier,
   },
   rules: {
     ...reactHooks.configs.recommended.rules,
@@ -24,5 +25,20 @@ export default tseslint.config({
       'warn',
       { allowConstantExport: true },
     ],
+    ...eslintConfigPrettier.rules,
+    "@typescript-eslint/naming-convention": [
+      "warn",
+      {
+        "selector": "variable",
+        "format": [
+          "camelCase",
+          "PascalCase",
+          "snake_case",
+          "UPPER_CASE"
+        ]
+      }
+    ],
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
   },
 })
