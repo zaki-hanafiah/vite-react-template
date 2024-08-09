7070b1fdff48091cc3b2564c5b731f769e7be49f
zaki-hanafiah <zaki@zakhov.com>
2024-08-10 01:01:58 +0800

    chore: add shadcn/ui and tailwind config

--- eslint.config.js
+++ eslint.config.js
@@ -3,42 +3,37 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import pluginPrettier from 'eslint-plugin-prettier'
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslintConfigPrettier from 'eslint-config-prettier'
 import tsEslint from 'typescript-eslint'
 
 export default tsEslint.config({
-  extends: [js.configs.recommended, ...tsEslint.configs.recommended],
-  files: ['**/*.{ts,tsx}'],
-  ignores: ['dist'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
-  },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-    'plugin-prettier': pluginPrettier,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    ...eslintConfigPrettier.rules,
-    "@typescript-eslint/naming-convention": [
-      "warn",
-      {
-        "selector": "variable",
-        "format": [
-          "camelCase",
-          "PascalCase",
-          "snake_case",
-          "UPPER_CASE"
-        ]
-      }
-    ],
-    "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/no-explicit-any": "warn",
-  },
+    extends: [js.configs.recommended, ...tsEslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist'],
+    languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+    },
+    plugins: {
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+        'plugin-prettier': pluginPrettier,
+    },
+    rules: {
+        ...reactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        ...eslintConfigPrettier.rules,
+        '@typescript-eslint/naming-convention': [
+            'warn',
+            {
+                selector: 'variable',
+                format: ['camelCase', 'PascalCase', 'snake_case', 'UPPER_CASE'],
+            },
+        ],
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
+    },
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import { ProfileForm } from '@/components/Form.tsx'
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
+            <h1>Vite + React + shadcn/ui</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
+                <ProfileForm />
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     )
 }
